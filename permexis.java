18aec6c866ac56143d9084ccbf85e91b5d332fbb
iamvatsal <vatsalkesarwani12@gmail.com>
2019-09-04 06:39:13 +0530

    learning features of git hub

--- permexis.java
+++ permexis.java
@@ -9,6 +9,130 @@ class Codechef
 {
     static class Reader     //fasest way to take input
 	{                        //refer geeksforgeeks for more info
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 848026443e0b64ed2b729cec50230b85cecb58e9
+		final private int BUFFER_SIZE = 1 << 16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer, bytesRead;
+
+		public Reader()
+		{
+			din = new DataInputStream(System.in);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public Reader(String file_name) throws IOException 
+		{
+			din = new DataInputStream(new FileInputStream(file_name));
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public String readLine() throws IOException
+		{
+			byte[] buf = new byte[(int)(Math.pow(10,5)+1)]; // line length
+			int cnt = 0, c;
+			while ((c = read()) != -1)
+			{
+				if (c == '\n')
+					break;
+				buf[cnt++] = (byte) c;
+			}
+			return new String(buf, 0, cnt);
+		}
+
+		public int nextInt() throws IOException
+		{
+			int ret = 0;
+			byte c = read();
+			while (c <= ' ')
+				c = read();
+			boolean neg = (c == '-');
+			if (neg)
+				c = read();
+			do
+			{
+				ret = ret * 10 + c - '0';
+			} while ((c = read()) >= '0' && c <= '9');
+
+			if (neg)
+				return -ret;
+			return ret;
+		}
+
+		public long nextLong() throws IOException
+		{
+			long ret = 0;
+			byte c = read();
+			while (c <= ' ')
+				c = read();
+			boolean neg = (c == '-');
+			if (neg)
+				c = read();
+			do {
+				ret = ret * 10 + c - '0';
+			}
+			while ((c = read()) >= '0' && c <= '9');
+			if (neg)
+				return -ret;
+			return ret;
+		}
+
+		public double nextDouble() throws IOException
+		{
+			double ret = 0, div = 1;
+			byte c = read();
+			while (c <= ' ')
+				c = read();
+			boolean neg = (c == '-');
+			if (neg)
+				c = read();
+
+			do {
+				ret = ret * 10 + c - '0';
+			}
+			while ((c = read()) >= '0' && c <= '9');
+
+			if (c == '.')
+			{
+				while ((c = read()) >= '0' && c <= '9')
+				{
+					ret += (c - '0') / (div *= 10);
+				}
+			}
+
+			if (neg)
+				return -ret;
+			return ret;
+		}
+
+		private void fillBuffer() throws IOException
+		{
+			bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			if (bytesRead == -1)
+				buffer[0] = -1;
+		}
+
+		private byte read() throws IOException
+		{
+			if (bufferPointer == bytesRead)
+				fillBuffer();
+			return buffer[bufferPointer++];
+		}
+
+		public void close() throws IOException
+		{
+			if (din == null)
+				return;
+			din.close();
+		}
+	}
+<<<<<<< HEAD
+=======
 		final private int BUFFER_SIZE = 1 << 16; 
 		private DataInputStream din; 
 		private byte[] buffer; 
@@ -127,6 +251,9 @@ class Codechef
 			din.close(); 
 		} 
 	} 
+>>>>>>> parent of 3a2e7db... changes made
+=======
+>>>>>>> 848026443e0b64ed2b729cec50230b85cecb58e9
 	public static void main (String[] args) throws java.lang.Exception
 	{
 	    BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new
